e30219406b312e0ae613eeba923c03581cd7c13a
Элина <allene2@yandex.ru>
2024-10-15 21:24:09 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import true_math as tr
+import fake_math as fk
+
+result1 = fk.divide(121, 11)
+result2 = fk.divide(12, 0)
+result3 = tr.divide(625, 25)
+result4 = tr.divide(17, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
